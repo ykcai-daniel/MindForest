d99a1620290094c81e82a9c75cdc76dab568aa09
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2022-03-11 18:10:54 +0800

    Add js file

---
+++ public/js/usercenter.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    const button=document.getElementsByTagName('button')
+
+    button[0].onclick=()=>{
+        const num=document.getElementById('demo')
+        num.innerText=(parseInt(num.innerText)+1).toString()
+    }
+
+    button[1].onclick=()=>{
+        const num=document.getElementById('demo')
+        num.style.color='green'
+    }
+};
